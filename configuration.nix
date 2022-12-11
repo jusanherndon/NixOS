fb33fe85ebadef3de17843948e86de18b6534c34
Justin Herndon <jherndon111@gmail.com>
2022-12-11 16:12:51 -0600

    adding a tmux config

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,7 @@
         oh-my-zsh
 	pulseaudio
 	home-manager
+        tmux
   
   ];
   };
@@ -102,6 +103,21 @@
         set nocompatible
         filetype off
       '';    
+    };
+    
+    programs.tmux = {
+    enable = true;
+    extraConfig = ''
+    # List of plugins
+    #set -g @plugin 'tmux-plugins/tpm'
+    #set -g @plugin 'tmux-plugins/tmux-sensible'
+    #set -g @plugin 'tmux-plugins/tmux-resurrect'
+    #set -g @plugin 'tmux-plugins/tmux-continuum'
+    #set -g @plugin 'tmux-plugins/tmux-sessionist'
+    #set -g @plugin 'tmux-plugins/tmux-prefix-highlight'
+    #set -g @plugin 'tmux-plugins/tmux-pain-control'
+    #set -g @plugin 'tmux-plugins/tmux-sidebar'
+    '';
     };
     
     programs.git = {
